8220e68a19fcfcf5216b756b0bf487e41a0a8c0d
Kaic Melo <kakakaic@yahoo.com.br>
2022-06-11 01:39:56 -0300

    projeto teste para popular opções

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+import {
+  PoDynamicFormField,
+  PoMenuItem,
+  PoSelectOption,
+  PoTableColumn,
+} from '@po-ui/ng-components';
+import { environment } from 'src/environments/environment';
+import { FormBuilder, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'testeLoad';
+export class AppComponent implements OnInit {
+  fields: any = [];
+  listaDeItens: any = [];
+  colunas: Array<PoTableColumn> = [
+    {
+      property: 'dataInicial',
+      label: 'Data Inicial',
+      type: 'date',
+      format: 'dd/MM/YYYY HH:mm',
+    },
+    {
+      property: 'dataFinal',
+      label: 'Data Final',
+      type: 'date',
+      format: 'dd/MM/YYYY HH:mm',
+    },
+    {
+      property: 'estado',
+      label: 'Estado',
+    },
+  ];
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) {}
+
+  async ngOnInit(): Promise<void> {
+    this.http
+      .get(`http://localhost:3000/items`)
+      .subscribe((respostaApi: any) => {
+        respostaApi.map((ListaEstados: any) => {
+          this.fields.push({
+            label: ListaEstados,
+            value: ListaEstados,
+          });
+        });
+      });
+  }
+
+  checkoutForm = this.formBuilder.group({
+    dataInicial: ['', Validators.required],
+    dataFinal: ['', Validators.required],
+    estado: ['', Validators.required],
+  });
+
+  readonly menus: Array<PoMenuItem> = [{ label: 'Home', action: () => {} }];
+
+  salvar() {
+    const itensSalvo = this.checkoutForm.getRawValue();
+
+    this.listaDeItens.push({...itensSalvo});
+
+    this.checkoutForm.reset();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PoButtonModule, PoDynamicModule, PoFieldModule, PoModule, PoTableModule } from '@po-ui/ng-components';
+import { RouterModule } from '@angular/router';
+import { PoTemplatesModule } from '@po-ui/ng-templates';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PoModule,
+    RouterModule.forRoot([]),
+    PoTemplatesModule,
+    PoDynamicModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    PoFieldModule,
+    PoButtonModule,
+    PoTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
